96f900bd4550095efac199ed9fc08723938c6224
Anoop Naravaram <anoop901@gmail.com>
2021-03-21 20:56:25 -0500

    Make "Create Task" button. Also adjust CSS so that the colors, borders, and spacings still look good.

--- imports/ui/TasksView.tsx
+++ imports/ui/TasksView.tsx
@@ -1,7 +1,6 @@
 import { useTracker } from "meteor/react-meteor-data";
 import React from "react";
 import { TaskCollection } from "../api/Task";
-import { NewTaskForm } from "./NewTaskForm";
 import { TaskList } from "./TaskList";
 import "./TasksView.css";
 
@@ -9,11 +8,9 @@ export function TasksView() {
   const tasks = useTracker(() => TaskCollection.find().fetch());
   return (
     <div className="TasksView">
-      <NewTaskForm
-        createTask={(name) => {
-          TaskCollection.insert({ name, state: "incomplete" });
-        }}
-      />
+      <div className="CreateTaskButtonRow">
+        <button className="CreateTaskButton">Create Task</button>
+      </div>
       <TaskList tasks={tasks} />
     </div>
   );
