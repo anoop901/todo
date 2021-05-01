c8d5777b5fa2b0faa461d371a64492f01036871f
Anoop Naravaram <anoop901@gmail.com>
2021-05-01 17:40:36 -0500

    Use Drawer component for sidebar
    This allows removal of a lot of custom logic, and also animates the
    drawer's appearance and disappearance.

--- imports/ui/TasksView.tsx
+++ imports/ui/TasksView.tsx
@@ -4,21 +4,20 @@ import { TaskCollection } from "../api/Task";
 import { TaskDetailsForm } from "./TaskDetailsForm";
 import { NewTaskForm } from "./NewTaskForm";
 import { TaskList } from "./TaskList";
-import { Box, Button, Hidden, Paper } from "@material-ui/core";
+import { Box, Button, Drawer } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    height: "100%",
-  },
-  main: {
-    overflow: "auto",
-  },
   sidebar: {
-    overflow: "auto",
+    width: `${theme.breakpoints.values.md * 0.5}px`,
+    [theme.breakpoints.down("sm")]: {
+      width: "50%",
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "100%",
+    },
   },
   controlRow: {
     "& > *": {
@@ -35,68 +34,54 @@ export function TasksView() {
   >(null);
   const classes = useStyles();
   return (
-    <Grid container wrap="nowrap" direction="row" className={classes.root}>
-      <Hidden xsDown={currentMenu !== null}>
-        <Grid
-          item
-          container
-          wrap="nowrap"
-          direction="column"
-          xs={12}
-          sm={currentMenu === null ? 12 : 6}
-          md={currentMenu === null ? 12 : 8}
-          className={classes.main}
+    <>
+      <Box display="flex" alignItems="center" className={classes.controlRow}>
+        <Button
+          variant="contained"
+          color="primary"
+          disabled={currentMenu === "NewTask"}
+          onClick={() => {
+            setCurrentMenu("NewTask");
+            setSelectedTaskId(null);
+          }}
+          startIcon={<AddIcon />}
         >
-          <Box
-            display="flex"
-            alignItems="center"
-            className={classes.controlRow}
-          >
-            <Button
-              variant="contained"
-              color="primary"
-              disabled={currentMenu === "NewTask"}
-              onClick={() => {
-                setCurrentMenu("NewTask");
-                setSelectedTaskId(null);
-              }}
-              startIcon={<AddIcon />}
-            >
-              New Task
-            </Button>
-            <Box flex={1} />
-          </Box>
-          <TaskList
-            tasks={tasks}
-            selectedTaskId={selectedTaskId}
-            setSelectedTaskId={(newSelectedTaskId) => {
-              setCurrentMenu("TaskDetails");
-              setSelectedTaskId(newSelectedTaskId);
+          New Task
+        </Button>
+        <Box flex={1} />
+      </Box>
+      <TaskList
+        tasks={tasks}
+        selectedTaskId={selectedTaskId}
+        setSelectedTaskId={(newSelectedTaskId) => {
+          setCurrentMenu("TaskDetails");
+          setSelectedTaskId(newSelectedTaskId);
+        }}
+      />
+      <Drawer
+        variant="persistent"
+        anchor="right"
+        open={currentMenu !== null}
+        PaperProps={{ elevation: 16, className: classes.sidebar }}
+      >
+        {currentMenu === "NewTask" ? (
+          <NewTaskForm
+            closeForm={() => {
+              setCurrentMenu(null);
+              setSelectedTaskId(null);
             }}
           />
-        </Grid>
-      </Hidden>
-      <Hidden xsUp={currentMenu === null}>
-        <Grid item xs={12} sm={6} md={4} className={classes.sidebar} component={Paper} square elevation={12}>
-          {currentMenu === "NewTask" ? (
-            <NewTaskForm
-              closeForm={() => {
-                setCurrentMenu(null);
-                setSelectedTaskId(null);
-              }}
-            />
-          ) : null}
-          {currentMenu === "TaskDetails" && selectedTaskId !== null ? (
-            <TaskDetailsForm
-              task={TaskCollection.findOne({ _id: selectedTaskId })!}
-              closeForm={() => {
-                setCurrentMenu(null);
-                setSelectedTaskId(null);
-              }}
-            />
-          ) : null}
-        </Grid>
-      </Hidden>
-    </Grid>
+        ) : null}
+        {currentMenu === "TaskDetails" && selectedTaskId !== null ? (
+          <TaskDetailsForm
+            task={TaskCollection.findOne({ _id: selectedTaskId })!}
+            closeForm={() => {
+              setCurrentMenu(null);
+              setSelectedTaskId(null);
+            }}
+          />
+        ) : null}
+      </Drawer>
+    </>
   );
 }
