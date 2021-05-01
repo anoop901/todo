92dfad952b281fbb8e62618919b7247046ad24bd
Anoop Naravaram <anoop901@gmail.com>
2021-05-01 10:15:42 -0500

    Select name field when selecting a task

--- imports/ui/TaskConfigInputs.tsx
+++ imports/ui/TaskConfigInputs.tsx
@@ -14,6 +14,7 @@ export function TaskConfigInputs({
   const nameInputRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
     nameInputRef.current?.focus();
+    nameInputRef.current?.select();
   }, [task._id]);
   return (
     <>
