96868059670d40ab08957b3f95d734fbd12fbcfd
Matthew Hammond <matthewbh95@gmail.com>
2021-06-16 01:29:24 -0500

    Add postpone and delete actions to tasks in the task list view.
    Postpone opens a popup menu and allows to you add +1h, +1d, or +1w to a task.
    
    Both buttons are hidden until you mouse over a task.

--- imports/ui/TaskListItem.tsx
+++ imports/ui/TaskListItem.tsx
@@ -1,9 +1,13 @@
 import {
   Checkbox,
+  IconButton,
   ListItem,
   ListItemIcon,
+  ListItemSecondaryAction,
   ListItemText,
   makeStyles,
+  Menu,
+  MenuItem,
 } from "@material-ui/core";
 import classNames from "classnames";
 import format from "date-fns/format";
@@ -12,6 +16,8 @@ import React from "react";
 import { Task } from "../db/Task";
 import { useAppDispatch } from "./reducers/hooks";
 import { setSelectedTask } from "./reducers/tasksViewSlice";
+import DeleteIcon from "@material-ui/icons/Delete";
+import UpdateIcon from "@material-ui/icons/Update";
 
 const useStyles = makeStyles((theme) => ({
   droppedCheckbox: { visibility: "hidden" },
@@ -20,6 +26,19 @@ const useStyles = makeStyles((theme) => ({
     textDecorationLine: "line-through",
   },
   completeText: { color: theme.palette.text.disabled },
+  listItemSecondaryAction: {
+    visibility: "hidden"
+  },
+  listItem: {
+    "&:hover $listItemSecondaryAction": {
+      visibility: "inherit",
+    }
+  },
+  listItemSecondaryActionSpacer: {
+    width: "25px",
+    height: "auto",
+    display: "inline-block",
+  }
 }));
 
 export function TaskListItem({
@@ -31,12 +50,35 @@ export function TaskListItem({
 }): JSX.Element {
   const classes = useStyles();
   const dispatch = useAppDispatch();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const postponeClicked = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const closePostponeMenu = () => {
+    setAnchorEl(null);
+  };
+  const postponeBy = (hoursToPostponeBy: number) => function () {
+    if (task.plannedDate === undefined) {
+      return;
+    }
+    const newDate = new Date(task.plannedDate.getTime());
+    newDate.setHours(task.plannedDate.getHours() + hoursToPostponeBy);
+
+    Meteor.call("tasks.setPlannedDate", task._id, newDate);
+    closePostponeMenu();
+  }
+
   return (
     <ListItem
       button
       selected={selected}
       onClick={() => {
         dispatch(setSelectedTask(task._id));
+      }}
+      classes={{
+        container: classes.listItem
       }}
     >
       <ListItemIcon>
@@ -70,6 +112,29 @@ export function TaskListItem({
           task.plannedDate != null ? format(task.plannedDate, "p") : undefined
         }
       />
+      <ListItemSecondaryAction className={classes.listItemSecondaryAction}>
+        <IconButton edge="end" aria-label="postpone" onClick={postponeClicked}
+          hidden={task.plannedDate === undefined}>
+          <UpdateIcon />
+        </IconButton>
+        <Menu
+          id="postpone-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={closePostponeMenu}
+        >
+          <MenuItem onClick={postponeBy(1)}>+1h</MenuItem>
+          <MenuItem onClick={postponeBy(24)}>+1d</MenuItem>
+          <MenuItem onClick={postponeBy(24 * 7)}>+1w</MenuItem>
+        </Menu>
+        <div className={classes.listItemSecondaryActionSpacer} />
+        <IconButton edge="end" aria-label="delete" onClick={() => {
+          Meteor.call("tasks.delete", task._id);
+        }}>
+          <DeleteIcon />
+        </IconButton>
+      </ListItemSecondaryAction>
     </ListItem>
   );
 }
